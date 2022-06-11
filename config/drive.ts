da81d5deac59f46193304bfa6cf5cbf56f8ed84d
Moaz Amin <moaz@vfairs.com>
2022-06-11 23:58:47 +0500

    code reformate

--- config/drive.ts
+++ config/drive.ts
@@ -6,7 +6,7 @@
  */
 
 import Env from '@ioc:Adonis/Core/Env'
-import { driveConfig } from '@adonisjs/core/build/config'
+import {driveConfig} from '@adonisjs/core/build/config'
 import Application from '@ioc:Adonis/Core/Application'
 
 /*
@@ -125,24 +125,24 @@ export default driveConfig({
     //   keyFilename: Env.get('GCS_KEY_FILENAME'),
     //   bucket: Env.get('GCS_BUCKET'),
 
-      /*
-      |--------------------------------------------------------------------------
-      | Uniform ACL - Google cloud storage only
-      |--------------------------------------------------------------------------
-      |
-      | When using the Uniform ACL on the bucket, the "visibility" option is
-      | ignored. Since, the files ACL is managed by the google bucket policies
-      | directly.
-      |
-      |**************************************************************************
-      | Learn more: https://cloud.google.com/storage/docs/uniform-bucket-level-access
-      |**************************************************************************
-      |
-      | The following option just informs drive whether your bucket is using uniform
-      | ACL or not. The actual setting needs to be toggled within the Google cloud
-      | console.
-      |
-      */
+    /*
+    |--------------------------------------------------------------------------
+    | Uniform ACL - Google cloud storage only
+    |--------------------------------------------------------------------------
+    |
+    | When using the Uniform ACL on the bucket, the "visibility" option is
+    | ignored. Since, the files ACL is managed by the google bucket policies
+    | directly.
+    |
+    |**************************************************************************
+    | Learn more: https://cloud.google.com/storage/docs/uniform-bucket-level-access
+    |**************************************************************************
+    |
+    | The following option just informs drive whether your bucket is using uniform
+    | ACL or not. The actual setting needs to be toggled within the Google cloud
+    | console.
+    |
+    */
     //   usingUniformAcl: false,
     // },
   },
--- contracts/hash.ts
+++ contracts/hash.ts
@@ -5,9 +5,10 @@
  * file.
  */
 
-import { InferListFromConfig } from '@adonisjs/core/build/config'
+import {InferListFromConfig} from '@adonisjs/core/build/config'
 import hashConfig from '../config/hash'
 
 declare module '@ioc:Adonis/Core/Hash' {
-  interface HashersList extends InferListFromConfig<typeof hashConfig> {}
+  interface HashersList extends InferListFromConfig<typeof hashConfig> {
+  }
 }
