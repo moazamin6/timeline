d5596e410a60fe7485b61deb8fe69e1f8e980676
Moaz Amin <moaz@vfairs.com>
2022-06-11 23:59:29 +0500

    reformate code 2

--- test.ts
+++ test.ts
@@ -16,17 +16,17 @@ process.env.NODE_ENV = 'test'
 
 import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
-import { Ignitor } from '@adonisjs/core/build/standalone'
-import { configure, processCliArgs, run, RunnerHooksHandler } from '@japa/runner'
+import {Ignitor} from '@adonisjs/core/build/standalone'
+import {configure, processCliArgs, run, RunnerHooksHandler} from '@japa/runner'
 
-sourceMapSupport.install({ handleUncaughtExceptions: false })
+sourceMapSupport.install({handleUncaughtExceptions: false})
 
 const kernel = new Ignitor(__dirname).kernel('test')
 
 kernel
   .boot()
   .then(() => import('./tests/bootstrap'))
-  .then(({ runnerHooks, ...config }) => {
+  .then(({runnerHooks, ...config}) => {
     const app: RunnerHooksHandler[] = [() => kernel.start()]
 
     configure({
